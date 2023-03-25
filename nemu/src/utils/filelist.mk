322a0272213f3adc86975bf3687c37e328e5d42b
tracer-ics2022 <tracer@njuics.org>
2023-03-25 14:39:38 +0800

    >  compile NEMU 211220000 张三 Linux DESKTOP-2VFSK7Q 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:39:38 up 32 min,  0 users,  load average: 0.19, 0.12, 0.10

--- nemu/src/utils/filelist.mk
+++ nemu/src/utils/filelist.mk
@@ -15,6 +15,6 @@
 
 ifdef CONFIG_ITRACE
 CXXSRC = src/utils/disasm.cc
-CXXFLAGS += $(shell llvm-config --cxxflags) -fPIE
+CXXFLAGS += $(shell llvm-config-14 --cxxflags) -fPIE
 LIBS += $(shell llvm-config --libs)
 endif
