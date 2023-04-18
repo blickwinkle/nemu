10c34f77f11fd45efd3e73d0e23f84d64e9c0bd2
tracer-ics2022 <tracer@njuics.org>
2023-04-18 21:21:34 +0800

    >  compile NEMU 211220000 张三 Linux DESKTOP-2VFSK7Q 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:21:34 up  3:12,  0 users,  load average: 0.38, 0.11, 0.05

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -21,7 +21,7 @@
 #include <regex.h>
 
 enum {
-  TK_NOTYPE = 256, TK_EQ,
+  TK_NOTYPE = 256, TK_EQ, TK_NUM,
 
   /* TODO: Add more token types */
 
@@ -39,6 +39,14 @@ static struct rule {
   {" +", TK_NOTYPE},    // spaces
   {"\\+", '+'},         // plus
   {"==", TK_EQ},        // equal
+
+  {"\\-", '-'},         // plus
+  {"\\*", '*'},         // plus
+  {"\\/", '/'},         // plus
+  {"\\(", '('},         // plus
+  {"\\)", ')'},         // plus
+
+  {"^(\\-|\\+)?\\d+(\\.\\d+)?$", TK_NUM},         // plus
 };
 
 #define NR_REGEX ARRLEN(rules)
@@ -95,6 +103,28 @@ static bool make_token(char *e) {
          */
 
         switch (rules[i].token_type) {
+          case TK_NOTYPE : {
+
+          } break ;
+          case '+' : {
+            tokens[nr_token++].type = '+';
+          } break ;
+          case '-' : {
+            tokens[nr_token++].type = '-';
+          } break ;
+          case '*' : {
+            tokens[nr_token++].type = '*';
+          } break ;
+          case '/' : {
+            tokens[nr_token++].type = '/';
+          } break ;
+          case TK_EQ : {
+            tokens[nr_token++].type = TK_EQ;
+          } break ;
+          case TK_NUM : {
+            tokens[nr_token].type = TK_NUM;
+            strncpy(tokens[nr_token++].str, substr_start, substr_len);
+          } break ;
           default: TODO();
         }
 
@@ -112,13 +142,93 @@ static bool make_token(char *e) {
 }
 
 
+static int check_parentheses(int p, int q) {
+  int ind = 0;
+  for (int i = p; i <= q; i++) {
+    if (tokens[i].type == '(') {
+      ind++;
+    } else if (tokens[i].type == ')') {
+      if (ind == 0) return false;
+      ind --;
+    }
+  }
+  return ind == 0 ? true : false;
+}
+
+static int findMainOp(int p, int q) {
+  int ind = 0;
+  int ret = -1;
+  for (int i = p; i <= q; i++)
+  {
+    if (tokens[i].type == '(')
+    {
+      ind++;
+      continue;
+    }
+    else if (tokens[i].type == ')')
+    {
+      ind--;
+      continue;
+    }
+    if (tokens[i].type == TK_NUM || ind != 0) {
+      continue ;
+    }
+    switch (tokens[i].type)
+    {
+    case '+':
+    case '-': {
+      ret = i;
+    } break ;
+
+    case '*':
+    case '/': {
+      if (ret == -1 || tokens[ret].type == '*' || tokens[ret].type == '/') {
+        ret = i;
+      }
+    } break;
+
+    default:
+      break;
+    }
+  }
+  return ret;
+}
+
+word_t eval(int p, int q) {
+  if (p > q) return 0;
+  else if (p == q) {
+    if (tokens[p].type != TK_NUM) {
+      return 0;
+    }
+    return atoi(tokens[p].str);
+  } else if (tokens[p].type == '(' && tokens[p].type == ')' && check_parentheses(p + 1, q - 1) == true) {
+    return eval(p + 1, q - 1);
+  } else {
+    int mop = findMainOp(p, q);
+    assert(mop != -1);
+    word_t val1 = eval(p, mop - 1);
+    word_t val2 = eval(mop + 1, q);
+
+    switch (tokens[mop].type) {
+      case '+': return val1 + val2;
+      case '-': return val1 - val2;
+      case '*': return val1 * val2;
+      case '/': return val1 / val2;
+      default: assert(0);
+    }
+  }
+}
+
 word_t expr(char *e, bool *success) {
-  if (!make_token(e)) {
+  if (!make_token(e) || check_parentheses(0, nr_token - 1) == false) {
     *success = false;
     return 0;
   }
 
   /* TODO: Insert codes to evaluate the expression. */
+  return eval(0, nr_token - 1);
+  
+  
   TODO();
 
   return 0;
