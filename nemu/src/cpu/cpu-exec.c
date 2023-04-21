2b5b372c7e1c13b333d70ce798fd01ffd7c3716d
tracer-ics2022 <tracer@njuics.org>
2023-04-21 15:17:52 +0800

    >  compile NEMU 211220000 张三 Linux DESKTOP-2VFSK7Q 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:17:52 up  6:05,  0 users,  load average: 0.20, 0.08, 0.02

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -67,7 +67,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
   disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst.val, ilen);
   
-  puts("111");
+  puts(p);
 #endif
 }
 
