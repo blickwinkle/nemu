0314298cbc519ec9cc3c5d540f46526ab4fe0119
tracer-ics2022 <tracer@njuics.org>
2023-04-23 21:21:01 +0800

    >  compile NEMU 211220000 张三 Linux DESKTOP-2VFSK7Q 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:21:01 up  5:59,  0 users,  load average: 0.33, 0.20, 0.11

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -3,13 +3,20 @@
 
 static uint64_t boot_time = 0;
 
+static uint64_t read_time() {
+  uint32_t hi = inl(RTC_ADDR + 4);
+  uint32_t lo = inl(RTC_ADDR);
+  uint64_t time = ((uint64_t)hi << 32) | lo;
+  return time;
+}
+
 void __am_timer_init() {
-  boot_time = inl(RTC_ADDR);
+  boot_time = read_time();
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   // uptime->us = 0;
-  uptime->us = inl(RTC_ADDR);
+  uptime->us = read_time() - boot_time;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
