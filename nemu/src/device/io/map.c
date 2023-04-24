89e5521cf909110bc2444f842d2c364dee0db181
tracer-ics2022 <tracer@njuics.org>
2023-04-24 09:57:16 +0800

    >  compile NEMU 211220000 张三 Linux DESKTOP-2VFSK7Q 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:57:16 up 30 min,  0 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -53,6 +53,7 @@ void init_map() {
 }
 
 word_t map_read(paddr_t addr, int len, IOMap *map) {
+  IFDEF(CONFIG_DTRACE, Log("read from " FMT_PADDR " with len = %d device name = %s", addr, len, map->name));
   assert(len >= 1 && len <= 8);
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
@@ -62,6 +63,7 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
 }
 
 void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
+  IFDEF(CONFIG_DTRACE, Log("write to " FMT_PADDR " with len = %d device name = %s", addr, len, map->name));
   assert(len >= 1 && len <= 8);
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
