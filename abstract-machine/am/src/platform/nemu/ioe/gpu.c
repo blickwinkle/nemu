341c7df836ba469fecbf171993d5165a4172d65d
tracer-ics2022 <tracer@njuics.org>
2023-04-24 12:13:21 +0800

    >  compile NEMU 211220000 张三 Linux DESKTOP-2VFSK7Q 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:13:21 up  2:46,  0 users,  load average: 0.07, 0.13, 0.09

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,17 +1,19 @@
 #include "riscv/riscv.h"
 #include <am.h>
 #include <nemu.h>
+#include <stdint.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
-static int w = 0, h = 0;
+
+static int width = 0, height = 0;
 
 void __am_gpu_init() {
   int i;
-  w = inl(VGACTL_ADDR) >> 16; // TODO: get the correct width
-  h = inl(VGACTL_ADDR) & 0xffff; // TODO: get the correct height
+  width = inl(VGACTL_ADDR) >> 16; // TODO: get the correct width
+  height = inl(VGACTL_ADDR) & 0xffff; // TODO: get the correct height
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < w * h; i++)
+  for (i = 0; i < width * height; i++)
     fb[i] = i;
   outl(SYNC_ADDR, 1);
 }
@@ -19,15 +21,26 @@ void __am_gpu_init() {
 void  __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = w, .height = h,
+    .width = width, .height = height,
     .vmemsz = 0
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  // AM_GPU_FBDRAW, AM帧缓冲控制器, 可写入绘图信息, 向屏幕(x, y)坐标处绘制w*h的矩形图像. 图像像素按行优先方式存储在pixels中, 每个像素用32位整数以00RRGGBB的方式描述颜色. 若sync为true, 则马上将帧缓冲中的内容同步到屏幕上.
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  int i, j;
+  for (i = 0; i < h; i++) {
+    for (j = 0; j < w; j++) {
+      fb[(y + i) * width + x + j] = ((uint32_t *)(ctl->pixels))[i * w + j];
+    }
+  }
+
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
+  
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
