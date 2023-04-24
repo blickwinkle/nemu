c2f9bee3859a16b500df4de4f00d5eec469e20a3
tracer-ics2022 <tracer@njuics.org>
2023-04-24 17:03:38 +0800

    >  compile NEMU 211220000 张三 Linux DESKTOP-2VFSK7Q 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:03:38 up  7:36,  0 users,  load average: 0.48, 0.26, 0.14

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -68,38 +68,38 @@ SDLCALL void SDL_Callback(void *userdata, uint8_t *stream, int len) {
   // if (len > nread) memset(stream + nread, 0, len - nread);
 }
 
-// static void _init_audio() {
-//   SDL_AudioSpec s = {};
-//   s.format = AUDIO_S16SYS; // 假设系统中音频数据的格式总是使用16位有符号数来表示
-//   s.userdata = NULL; // 不使用
-//   s.callback = SDL_Callback;
-//   s.freq = audio_base[reg_freq];
-//   s.channels = audio_base[reg_channels];
-//   s.samples = audio_base[reg_samples];
-//   SDL_InitSubSystem(SDL_INIT_AUDIO);
-//   SDL_OpenAudio(&s, NULL);
-//   SDL_PauseAudio(0);
+static void _init_audio() {
+  SDL_AudioSpec s = {};
+  s.format = AUDIO_S16SYS; // 假设系统中音频数据的格式总是使用16位有符号数来表示
+  s.userdata = NULL; // 不使用
+  s.callback = SDL_Callback;
+  s.freq = audio_base[reg_freq];
+  s.channels = audio_base[reg_channels];
+  s.samples = audio_base[reg_samples];
+  SDL_InitSubSystem(SDL_INIT_AUDIO);
+  SDL_OpenAudio(&s, NULL);
+  SDL_PauseAudio(0);
 
-//   audio_base[reg_sbuf_size] = CONFIG_SB_SIZE;
-//   audio_base[reg_count] = 0;
-//   audio_init = true;
-// }
+  audio_base[reg_sbuf_size] = CONFIG_SB_SIZE;
+  audio_base[reg_count] = 0;
+}
 
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
   if (offset == reg_init * sizeof(uint32_t) && len == 4 && is_write) {
-    SDL_AudioSpec s = {};
-    s.freq = audio_base[reg_freq];
-    s.format = AUDIO_S16SYS;
-    s.channels = audio_base[reg_channels];
-    s.samples = audio_base[reg_samples];
-    s.callback = SDL_Callback;
-    s.userdata = NULL;
+    // SDL_AudioSpec s = {};
+    // s.freq = audio_base[reg_freq];
+    // s.format = AUDIO_S16SYS;
+    // s.channels = audio_base[reg_channels];
+    // s.samples = audio_base[reg_samples];
+    // s.callback = SDL_Callback;
+    // s.userdata = NULL;
 
-    tail = 0;
-    audio_base[reg_count] = 0;
-    SDL_InitSubSystem(SDL_INIT_AUDIO);
-    SDL_OpenAudio(&s, NULL);
-    SDL_PauseAudio(0);
+    // tail = 0;
+    // audio_base[reg_count] = 0;
+    // SDL_InitSubSystem(SDL_INIT_AUDIO);
+    // SDL_OpenAudio(&s, NULL);
+    // SDL_PauseAudio(0);
+    _init_audio();
   }
 }
 
