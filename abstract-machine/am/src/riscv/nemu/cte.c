8a367f5c53eacfded8e87e4c7468a64dac1e0673
tracer-ics2022 <tracer@njuics.org>
2023-04-26 17:24:32 +0800

    >  compile NEMU 211220000 张三 Linux DESKTOP-2VFSK7Q 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:24:32 up  4:31,  0 users,  load average: 0.50, 0.30, 0.19

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -5,6 +5,8 @@
 static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
+  printf("Got interrupt: %lu, status: %lu, mepc: %lu\n", c->mcause, c->mstatus, c->mepc);
+  
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
