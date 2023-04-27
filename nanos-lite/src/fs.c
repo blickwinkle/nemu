089c966cd9f76850a40ae5d4c4f5a564558a8210
tracer-ics2022 <tracer@njuics.org>
2023-04-27 20:29:13 +0800

    >  compile NEMU 211220000 张三 Linux DESKTOP-2VFSK7Q 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:29:13 up 15:15,  0 users,  load average: 0.67, 0.44, 0.32

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -7,6 +7,7 @@ typedef struct {
   char *name;
   size_t size;
   size_t disk_offset;
+  size_t open_offset;
   ReadFn read;
   WriteFn write;
 } Finfo;
@@ -18,19 +19,68 @@ size_t invalid_read(void *buf, size_t offset, size_t len) {
   return 0;
 }
 
+
 size_t invalid_write(const void *buf, size_t offset, size_t len) {
   panic("should not reach here");
   return 0;
 }
 
+size_t std_write(const void *buf, size_t offset, size_t len) {
+  for (int i = 0; i < len; i++) {
+    putch(((char *)buf)[i]);
+  }
+  return len;
+}
+
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
+  [FD_STDIN]  = {"stdin", 0, 0, 0, invalid_read, invalid_write},
+  [FD_STDOUT] = {"stdout", 0, 0, 0, invalid_read, std_write},
+  [FD_STDERR] = {"stderr", 0, 0, 0, invalid_read, std_write},
 #include "files.h"
 };
 
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+
 void init_fs() {
   // TODO: initialize the size of /dev/fb
+  for (int i = 3; i < sizeof(file_table) / sizeof(file_table[0]); i++) {
+    file_table[i].disk_offset = file_table[i - 1].disk_offset + file_table[i - 1].size;
+    file_table[i].open_offset = 0;
+    file_table[i].read = ramdisk_read;
+    file_table[i].write = ramdisk_write;
+  }
+}
+
+int fs_open(const char *pathname, int flags, int mode) {
+  for (int i = 0; i < sizeof(file_table) / sizeof(file_table[0]); i++) {
+    if (strcmp(pathname, file_table[i].name) == 0) {
+      return i;
+    }
+  }
+  panic("failed to open file %s", pathname);
+  return -1;
+}
+size_t fs_read(int fd, void *buf, size_t len) {
+  size_t ret = file_table[fd].read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+  file_table[fd].open_offset += ret;
+  return ret;
+}
+size_t fs_write(int fd, const void *buf, size_t len) {
+  size_t ret = file_table[fd].write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+  file_table[fd].open_offset += ret;
+  return ret;
+}
+size_t fs_lseek(int fd, size_t offset, int whence) {
+  switch (whence) {
+    case SEEK_SET: file_table[fd].open_offset = offset; break;
+    case SEEK_CUR: file_table[fd].open_offset += offset; break;
+    case SEEK_END: file_table[fd].open_offset = file_table[fd].size + offset; break;
+    default: panic("invalid whence = %d", whence);
+  }
+  return file_table[fd].open_offset;
+}
+int fs_close(int fd) {
+  return 0;
 }
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,5 +1,6 @@
 #include <proc.h>
 #include <elf.h>
+#include <fs.h>
 
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
@@ -18,26 +19,36 @@ size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 // 返回ramdisk的大小, 单位为字节
 size_t get_ramdisk_size();
 
+int fs_open(const char *pathname, int flags, int mode);
+size_t fs_read(int fd, void *buf, size_t len);
+size_t fs_write(int fd, const void *buf, size_t len);
+size_t fs_lseek(int fd, size_t offset, int whence);
+int fs_close(int fd);
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
 
   //你需要找出每一个需要加载的segment的Offset, VirtAddr, FileSiz和MemSiz这些参数. 其中相对文件偏移Offset指出相应segment的内容从ELF文件的第Offset字节开始, 在文件中的大小为FileSiz, 它需要被分配到以VirtAddr为首地址的虚拟内存位置, 在内存中它占用大小为MemSiz. 也就是说, 这个segment使用的内存就是[VirtAddr, VirtAddr + MemSiz)这一连续区间, 然后将segment的内容从ELF文件中读入到这一内存区间, 并将[VirtAddr + FileSiz, VirtAddr + MemSiz)对应的物理区间清零.
   // code:
+  int fd = fs_open(filename, 0, 0);
   Elf_Ehdr elf;
-  ramdisk_read(&elf, 0, sizeof(Elf_Ehdr));
+  fs_read(fd, &elf, sizeof(Elf_Ehdr));
   // 检查ELF文件的魔数
   assert(*(uint32_t *)elf.e_ident == 0x464c457f);
   // // 检查ISA
   // assert(elf.e_machine == EXPECT_TYPE);
   Elf_Phdr ph;
-  ramdisk_read(&ph, elf.e_phoff, sizeof(Elf_Phdr));
+  fs_lseek(fd, elf.e_phoff, SEEK_SET);
+  fs_read(fd, &ph, sizeof(Elf_Phdr));
   for (int i = 0; i < elf.e_phnum; i++) {
     if (ph.p_type == PT_LOAD) {
       // 从ELF文件中读取seg
       //code:
-      ramdisk_read((void *)ph.p_vaddr, ph.p_offset, ph.p_filesz);
+      fs_lseek(fd, ph.p_offset, SEEK_SET);
+      fs_read(fd, (void *)ph.p_vaddr, ph.p_filesz);
       memset((void *)(ph.p_vaddr + ph.p_filesz), 0, ph.p_memsz - ph.p_filesz);
     }
-    ramdisk_read(&ph, elf.e_phoff + (i + 1) * sizeof(Elf_Phdr), sizeof(Elf_Phdr));
+    fs_lseek(fd, elf.e_phoff + (i + 1) * sizeof(Elf_Phdr), SEEK_SET);
+    fs_read(fd, &ph, sizeof(Elf_Phdr));
   }
   return elf.e_entry;
 }
