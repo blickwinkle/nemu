35cd694dd147de4f3abb305067968aa5f089332b
tracer-ics2022 <tracer@njuics.org>
2023-04-27 13:08:18 +0800

    >  compile NEMU 211220000 张三 Linux DESKTOP-2VFSK7Q 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:08:18 up 10:19,  0 users,  load average: 0.29, 0.23, 0.21

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,10 +1,29 @@
 #include <common.h>
 #include "syscall.h"
+void sys_yield(Context *c);
+void sys_exit(Context *c);
+
+static void ((*syscalls[])(Context *c)) = {
+  [SYS_yield] = sys_yield,
+  [SYS_exit] = sys_exit,
+};
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
 
   switch (a[0]) {
+    case SYS_yield: 
+    case SYS_exit: 
+      syscalls[a[0]](c); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+}
+
+void sys_yield(Context *c) {
+  c->GPRx = 0;
+}
+
+void sys_exit(Context *c) {
+  halt(c->GPR2);
 }
