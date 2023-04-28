34683a2e5d9e1516aad38c727d68c49e0463d27a
tracer-ics2022 <tracer@njuics.org>
2023-04-28 22:10:20 +0800

    >  compile NEMU 211220000 张三 Linux DESKTOP-2VFSK7Q 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:10:20 up 1 day,  4:59,  0 users,  load average: 0.09, 0.17, 0.20

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -65,11 +65,17 @@ int fs_open(const char *pathname, int flags, int mode) {
   return -1;
 }
 size_t fs_read(int fd, void *buf, size_t len) {
+  if (file_table[fd].open_offset + len > file_table[fd].size) {
+    len = file_table[fd].size - file_table[fd].open_offset;
+  }
   size_t ret = file_table[fd].read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
   file_table[fd].open_offset += ret;
   return ret;
 }
 size_t fs_write(int fd, const void *buf, size_t len) {
+  if (file_table[fd].open_offset + len > file_table[fd].size) {
+    len = file_table[fd].size - file_table[fd].open_offset;
+  }
   size_t ret = file_table[fd].write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
   file_table[fd].open_offset += ret;
   return ret;
