b2aff07cb5d52cc3d41cd735df2b065d9e5b4107
tracer-ics2022 <tracer@njuics.org>
2023-04-28 11:34:03 +0800

    >  compile NEMU 211220000 张三 Linux DESKTOP-2VFSK7Q 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:34:03 up 18:28,  0 users,  load average: 0.04, 0.12, 0.18

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -1,21 +1,16 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <sys/time.h>
+#include <NDL.h>
 
 int main() {
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    printf("tv_sec: %ld\n", tv.tv_sec);
-    printf("tv_usec: %ld\n", tv.tv_usec);
-    //通过gettimeofday()获取当前时间, 并每过0.5秒输出一句话.
+    uint32_t start = NDL_GetTicks();
+    //NDL_GetTicks()获取当前时间, 并每过0.5秒输出一句话.
     while(1) {
-        struct timeval tv1;
-        gettimeofday(&tv1, NULL);
-        uint64_t us = (tv1.tv_sec - tv.tv_sec) * 1000000 + tv1.tv_usec - tv.tv_usec;
-        if(us >= 500000) {
-            printf("tv_sec: %ld\n", tv1.tv_sec);
-            printf("tv_usec: %ld\n", tv1.tv_usec);
-            tv = tv1;
+        uint32_t now = NDL_GetTicks();
+        if (now - start >= 500) {
+            printf("Hello, world!\n");
+            start = now;
         }
     }
     return 0;
