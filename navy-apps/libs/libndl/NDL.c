5e8b18cf454841dc1135e8b69f46e603aa336273
tracer-ics2022 <tracer@njuics.org>
2023-04-28 21:31:45 +0800

    >  compile NEMU 211220000 张三 Linux DESKTOP-2VFSK7Q 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:31:44 up 1 day,  4:21,  0 users,  load average: 0.85, 0.51, 0.29

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -79,6 +79,7 @@ void NDL_OpenCanvas(int *w, int *h) {
 }
 
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
+  printf("NDL_DrawRect: x = %d, y = %d, w = %d, h = %d\n", x, y, w, h);
   for (int i = 0; i < h; i ++) {
     lseek(fbdev, (y + i) * screen_w * sizeof(uint32_t) + x * sizeof(uint32_t), SEEK_SET);
     // printf("NDL_DrawRect: lseek = %d\n", ((y + i) * screen_w * sizeof(uint32_t) + x * sizeof(uint32_t)) / 4);
