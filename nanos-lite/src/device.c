a308fc41328a2348ff587bf65f50e7a27f5fa7d3
tracer-ics2022 <tracer@njuics.org>
2023-04-29 19:28:53 +0800

    >  compile NEMU 211220000 张三 Linux DESKTOP-2VFSK7Q 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:28:53 up 1 day, 12:29,  0 users,  load average: 0.26, 0.18, 0.18

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -50,7 +50,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 size_t fb_write(const void *buf, size_t offset, size_t len) {
   //code:
   AM_GPU_MEMCPY_T info = {
-    .dest =  offset / sizeof(uint32_t),
+    .dest =  offset,
     .src = buf,
     .size = len / sizeof(uint32_t)
   };
