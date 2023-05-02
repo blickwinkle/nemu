47a88e3de82b83b9e05ddceab708e9d0d0ac527f
tracer-ics2022 <tracer@njuics.org>
2023-05-02 15:51:41 +0800

    >  compile NEMU 211220000 张三 Linux DESKTOP-2VFSK7Q 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:51:41 up 1 day, 18:38,  0 users,  load average: 0.46, 0.19, 0.07

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -30,12 +30,22 @@ void  __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 
 void __am_gpu_memcpy(AM_GPU_MEMCPY_T *req) {
   // AM_GPU_MEMCPY, AM帧缓冲控制器, 可写入绘图信息, 将pixels中的w*h个像素复制到屏幕(x, y)坐标处. 图像像素按行优先方式存储在pixels中, 每个像素用32位整数以00RRGGBB的方式描述颜色.
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  fb += req->dest;
-  for (int i = 0; i < req->size; i++) {
-    fb[i] = ((uint32_t *)(req->src))[i];
+  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  // fb += req->dest;
+  // for (int i = 0; i < req->size; i++) {
+  //   fb[i] = ((uint32_t *)(req->src))[i];
+  // }
+  // outl(SYNC_ADDR, 1);
+
+  uint32_t *src = (uint32_t *)req->src, *dst = (uint32_t *)(FB_ADDR + req->dest);
+  for (int i = 0; i < req->size >> 2; i++, src++, dst++){
+    *dst = *src;
   }
-  outl(SYNC_ADDR, 1);
+  char *c_src = (char *)src, *c_dst = (char *)dst;
+  for (int i = 0; i < (req->size & 3); i++){
+    c_dst[i] = c_src[i];
+  }
+
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
